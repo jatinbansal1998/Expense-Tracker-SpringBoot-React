24582babff2d5ef57a116fb77eac03c58ac43c42
Jatin Bansal <bansal.jatin1998@gmail.com>
2020-07-05 19:54:19 +0530

    setting up project

---
+++ src/main/java/com/jatin/ExpenseTrackerSpringBootReact/ExpenseTrackerSpringBootReactApplication.java
@@ -0,0 +1,13 @@
+package com.jatin.ExpenseTrackerSpringBootReact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseTrackerSpringBootReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseTrackerSpringBootReactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jatin/ExpenseTrackerSpringBootReact/ExpenseTrackerSpringBootReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jatin.ExpenseTrackerSpringBootReact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerSpringBootReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
